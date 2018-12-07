d213680d227aac0360b7b12d2d87d8b52c5a3276
Sheriel <sheriel.henry@bison.howard.edu>
2018-12-06 21:13:15 -0500

    Initial commit

---
+++ src/test/java/com/example/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String LoadFormPage() {
+        return "formtemplate";
+    }
+
+
+    @RequestMapping("/processform")
+    public String loadFromPage(@RequestParam("login") String Login,
+                               Model model) {
+        model.addAttribute("loginval", Login);
+        return "confirm";
+    }
+}
